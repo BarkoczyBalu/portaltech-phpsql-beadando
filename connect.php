65ea24685b1b32fefd0f24daa054dff58caf0ef2
Balázs Barkóczy <balazs.barkoczy@stud.uni-corvinus.hu>
2021-11-28 11:08:28 +0100

    utf-8_solution - Balu - 1128

--- connect.php
+++ connect.php
@@ -16,12 +16,12 @@ $kapcsolat = mysqli_connect("127.0.0.1", "blan_H0E9DL", "Hfe3_56");
 if (!$kapcsolat){
 die('Nem lehet csatlakozni a MySQL kiszolgálóhoz! '.mysqli_error());
 }
-/*else{
-print "Sikerült csatlakozni.<br>";}*/
+else{
+print "Sikerült csatlakozni.<br>";}
 /*Adatbázis kiválasztása*/
 mysqli_select_db( $kapcsolat, $adatbazis ) 
 or die ( "Nem lehet megnyitni a következő adatbázist: $adatbazis".mysqli_error());
-/*print "Sikeresen kiválasztott adatbázis: " .$adatbazis."<br>";*/
+print "Sikeresen kiválasztott adatbázis: " .$adatbazis."<br>";
 ?>
 </h1>
 </body>
